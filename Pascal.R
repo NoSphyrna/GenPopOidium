e06d2079b36cbcfb9d317d73de337c92f9c4a897
NoSphyrna <defranssupascal@outlook.fr>
2025-11-06 17:19:05 +0100

    Creat my file

---
+++ Pascal.R
@@ -0,0 +1,7 @@
+# ----------------------------- Projet Evolution de l'Oïdium -----------------------------
+
+# Paramètres initiaux :
+
+# Frequences alléliques initiales sous forme d'une liste :
+
+pY <- 
